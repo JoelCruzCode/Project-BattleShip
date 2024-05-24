213f420ba09b7f815fc2da337dc065b3a8a2d9d5
Joel Cruz <136981207+JoelCruzCode@users.noreply.github.com>
2024-05-24 03:44:44 -0700

    refactor: move and comment out unused code
    Moved and commented out unused code within index.js and moved it to
    helper.js for future reference. This change improves maintainability and
    readability.

---
+++ src/helper.js
@@ -0,0 +1,55 @@
+// unused functions
+// function populateShips(container, board) {
+//     container.forEach(row => {
+//         row.forEach(col => {
+//             if(board[row][col] === true) {
+
+//             }
+//         })
+//     })
+// }
+
+// content.textContent = "Hello Webpack";
+// console.log("hello?");
+// console.log(content);
+
+// import webpackImg from './assets/images/webpackImg.webp';
+// Webpack Image
+// const myIcon = new Image();
+// myIcon.src = webpackImg;
+// content.appendChild(myIcon);
+
+// function component() {
+//   const element = document.createElement("div");
+
+//   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+//   return element;
+// }
+
+// const player2 = new Player();
+// how can i get my player to change the state of vertical or not
+// const carrier = new BattleShip(5, true);
+// const battleship = new BattleShip(4, true);
+// const destroyer = new BattleShip(3, false);
+// const submarine = new BattleShip(3);
+// const patrolBoat = new BattleShip(2);
+
+// const content = document.querySelector('#content');
+// const title = document.createElement('h1');
+// title.textContent = 'BattleShip';
+// title.classList.add('greeting');
+// document.querySelector('body').prepend(title);
+
+// container1.addEventListener('click', (e) => {
+//   if (e.target.classList.contains('grid')) {
+//     // const cellClasses = e.target.classList;
+//     const index = e.target.dataset.value;
+//     console.log(index); // eslint-disable-line no-console
+//     // how do i find out which player class im clicking on  depending on board
+//     // call receive attack with index
+//     // handle interface depending on return of receive attack
+
+//     e.target.classList.add('hit');
+//   }
+// });
