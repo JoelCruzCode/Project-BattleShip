929ddeb8cf1c55335b996ac3e56bbfa380e23b3d
Joel Cruz <136981207+JoelCruzCode@users.noreply.github.com>
2024-05-24 03:37:42 -0700

    feat: create Player class
    Created a Player class that will hold a reference to a players name and
    build the GameBoard class in its constructor.

---
+++ src/player.js
@@ -0,0 +1,22 @@
+import Gameboard from './gameboard';
+
+class Player {
+  #board;
+
+  #name;
+
+  constructor(name) {
+    this.#board = new Gameboard();
+    this.name = name;
+  }
+
+  get board() {
+    return this.#board;
+  }
+
+  //   get name() {
+  //     return this.name;
+  //   }
+}
+
+export default Player;
