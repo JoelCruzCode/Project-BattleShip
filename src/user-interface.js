8847e487ea5e3409be0acdd23d211547d926f045
Joel Cruz <136981207+JoelCruzCode@users.noreply.github.com>
2024-06-04 15:56:05 -0700

    refactor: update hoverHandler and clean code space
    Updated the hoverHandler function in userInterface.js to work on both containers.
    Added a dataset index property to the mentioned container elements in
    index.html Removed unnecessary comments and cleared console logs.
    
    Changes include:
    Updated hoverHandler in userInterface.js
    Cleared logs in userInterface.js
    Updated dataset properties in index.html
    
    This commit improves code flexibility and readability

--- src/user-interface.js
+++ src/user-interface.js
@@ -7,10 +7,9 @@ import BattleShip from './battleship';
 import GameController from './game-controller';
 // import GameController from './game-controller';
 
-// don't think this is necessary just use normal removeEventListener
-function removeListener(type, element, listener) {
-  element.removeEventListener(type, listener);
-}
+// function removeListener(type, element, listener) {
+//   element.removeEventListener(type, listener);
+// }
 
 class UserInterface {
   constructor() {
@@ -20,8 +19,6 @@ class UserInterface {
   init() {
     // selectors
     this.nameBtn = document.querySelector('#player01-name-submit');
-    // this.verticalBtn = document.querySelector('#vertical-btn');
-    // this.horizontalBtn = document.querySelector('#horizontal-btn');
     this.startBtn = document.querySelector('#start-btn');
     this.resetBtn = document.querySelector('#reset-btn');
     this.description = document.querySelector('.description');
@@ -51,47 +48,173 @@ class UserInterface {
       event.preventDefault();
       const playerName = this.submitPlayerName(event);
       if (playerName) {
-        const { player1Name, player2Name, playerBoards } =
-          this.initializeGameController(playerName);
+        const { player1, player2 } = this.initializeGameController(playerName);
+        this.player1Name = player1.name;
+        this.player2Name = player2.name;
+        this.player1Board = player1.board.board;
+        this.player2Board = player2.board.board;
 
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
-        this.createGrids(playerBoards);
-        // current creating interface based on boards
-        // i need to change this to submit data based on ui to create boards
-
-        // need to create function to randomize cpus board
-        // then reflect this change since only player 1 grid will be selected
-        //
-        // this.createGrids(playerBoards);
+        this.createGrid(this.player1Board, this.container1);
       }
     });
 
     // Start game
     this.startBtn.addEventListener('click', this.startUserInterface);
 
-    this.axisContainer.addEventListener('click', (event) => {
-      this.invertFleetAxis(event);
-    });
+    // switch ship directions
+    this.axisContainer.addEventListener('click', this.invertFleetAxis);
 
-    this.fleetContainer.addEventListener('click', (event) => {
-      this.selectShip(event);
-    });
-
-    // Listen for attacks
-    this.container2.addEventListener('click', this.AttackCoordinator);
-
+    // Listen for ship placements
+    this.fleetContainer.addEventListener('click', this.selectShip);
     // Reset game
     this.resetBtn.addEventListener('click', this.resetUserInterface);
   }
 
+  initializeGameController(playerName) {
+    this.gameController = new GameController(playerName);
+
+    return this.gameController;
+  }
+
+  submitPlayerName(event) {
+    // Check if the input is empty
+    event.preventDefault();
+    if (!this.input.value.trim()) {
+      this.description.classList.remove('hidden');
+      this.description.textContent = 'Please enter a valid name';
+      return;
+    }
+
+    // Proceed with the form submission logic
+    this.playerTitle.textContent = this.input.value;
+    this.container1.classList.remove('hidden');
+    this.playerTitle.classList.remove('hidden');
+    this.form.classList.add('hidden');
+    this.aside.classList.remove('hidden');
+    this.description.textContent = '';
+    return this.input.value; // eslint-disable-line
+  }
+
+  createGrid(board, element) {
+    const container = element;
+    container.dataset.value =
+      container.dataset.index === 1 ? this.player1Name : this.player2Name;
+    for (let i = 0; i < board.length; i += 1) {
+      for (let j = 0; j < board.length; j += 1) {
+        const grid = document.createElement('div');
+        grid.classList.add('grid');
+        // indexing the user interface to reference the players board with event handlers later
+        grid.dataset.value = `[${i},${j}]`;
+        grid.style.gridTemplateColumns = `repeat(${board.length}, auto)`;
+        if (board[i][j] instanceof BattleShip) {
+          grid.classList.add('ship');
+        }
+        container.appendChild(grid);
+      }
+    }
+  }
+
+  startUserInterface = (event) => {
+    event.preventDefault();
+
+    // Check that user has placed all ships on board
+    if (this.placedShipsUI.length === 4) {
+      this.gameController.startGame();
+      this.createGrid(this.player2Board, this.container2);
+
+      this.startBtn.classList.add('hidden');
+      this.container2.classList.remove('hidden');
+      this.player2Title.classList.remove('hidden');
+      this.options.classList.add('hidden');
+
+      this.container1.removeEventListener('click', this.currentHandler);
+      this.container1.removeEventListener('mouseenter', this.mouseEnter, true);
+      this.container1.removeEventListener('mouseleave', this.mouseLeave, true);
+
+      this.mouseEnter = this.hoverHandler.bind(this, true);
+      this.mouseLeave = this.hoverHandler.bind(this, false);
+
+      // Add the new event listeners
+      this.shipData.size = 1;
+      this.container2.addEventListener('click', this.AttackCoordinator);
+      this.container2.addEventListener('mouseenter', this.mouseEnter, true);
+      this.container2.addEventListener('mouseleave', this.mouseLeave, true);
+    }
+  };
+
+  resetUserInterface = (event) => {
+    event.preventDefault();
+    this.startBtn.classList.toggle('hidden');
+    this.container2.classList.toggle('hidden');
+    this.player2Title.classList.toggle('hidden');
+    this.description.textContent = '';
+    this.resetBtn.classList.toggle('hidden');
+    this.gameController.resetGame();
+    this.player1Board = this.gameController.player1.board.board;
+    this.player2Board = this.gameController.cpu.board.board;
+    this.container1.innerHTML = ``;
+    this.container2.innerHTML = ``;
+    this.placedShipsUI = [];
+    this.createGrid(this.player1Board, this.container1);
+    this.axisContainer.addEventListener('click', this.invertFleetAxis);
+  };
+
+  gameOverUserInterface(attacker) {
+    this.description.textContent = `Game over, ${attacker.name} wins!`;
+    this.options.classList.remove('hidden');
+    this.resetBtn.classList.remove('hidden');
+    this.container1.removeEventListener('click', this.AttackCoordinator);
+    this.container2.removeEventListener('click', this.fleetContainer);
+    this.container2.removeEventListener('mouseenter', this.mouseEnter, true);
+    this.container2.removeEventListener('mouseleave', this.mouseLeave, true);
+    this.axisContainer.removeEventListener('click', this.invertFleetAxis);
+    this.shipData = null;
+  }
+
+  displayAttackResult(results, event) {
+    const { attack, attacker, fleetSunk, ship } = results;
+    this.description.textContent = attack;
+    if (attack === 'you already targeted this position, try again') {
+      console.log('move this clause');
+    } else if (attack === 'Successful hit') {
+      event.target.classList.add('hit'); // orange
+    } else if (attack === 'Ship sinking') {
+      // traverse the positions of the sinking ship to update the UI(red)
+      ship.positions.forEach((pos) => {
+        const [r, c] = pos;
+        const container = event.target.parentNode.id;
+        const gridEl = document.querySelector(
+          `#${container} [data-value="[${r},${c}]"]`,
+        );
+        gridEl.classList.add('sunk');
+      });
+    } else {
+      event.target.classList.add('miss');
+    }
+    if (fleetSunk) {
+      this.gameOverUserInterface(attacker);
+    }
+  }
+
+  AttackCoordinator = (event) => {
+    if (event.target.classList.contains('grid')) {
+      const index = event.target.dataset.value;
+      const playerName = event.target.parentNode.dataset.value;
+      const [row, column] = JSON.parse(index);
+      const results = this.gameController.attack(playerName, row, column);
+      this.displayAttackResult(results, event);
+    }
+  };
+
   hoverHandler = (isEntering, e) => {
     const { size, axis } = this.shipData;
-    const containerChildren = this.container1.children;
     if (e.target.classList.contains('grid')) {
       let index = e.target.dataset.value;
+      const containerChildren =
+        e.target.parentNode.dataset.index === '1'
+          ? this.container1.children
+          : this.container2.children;
       const [row, column] = JSON.parse(index);
-      // console.log('before loop', [row, column]);
       for (let i = 0; i < size; i += 1) {
         let r = row;
         let c = column;
@@ -182,8 +305,6 @@ class UserInterface {
           c = column + i;
         }
         index = `${r}${c}` < 10 ? c : `${r}${c}`;
-        console.log('index:', index);
-
         // make sure index is not out of bounds
 
         shipData.coords.push(index);
@@ -205,7 +326,7 @@ class UserInterface {
           containerChildren[coord].classList.add('ship'),
         );
         this.placedShipsUI.push(shipData);
-        console.log('zeShips:', this.placedShipsUI);
+        console.log('placedShipsUI:', this.placedShipsUI);
         return;
       }
       shipData.coords.splice(0, shipData.size);
@@ -227,7 +348,6 @@ class UserInterface {
       });
 
       if (this.currentHandler) {
-        console.log('Removing previous listeners');
         this.container1.removeEventListener('click', this.currentHandler);
         this.container1.removeEventListener(
           'mouseenter',
@@ -242,10 +362,8 @@ class UserInterface {
       }
       if (this.shipData) {
         this.shipData.axis = event.target.textContent.toLowerCase();
-        // const { size, axis } = this.shipData;
 
-        // Create a new handler with the current size and axis
-        console.log('The Axis is now', this.shipData.axis);
+        // Create new handlers with the current size and axis
         this.currentHandler = this.clickHandler.bind(this, this.shipData);
         this.mouseEnter = this.hoverHandler.bind(this, true);
         this.mouseLeave = this.hoverHandler.bind(this, false);
@@ -254,151 +372,6 @@ class UserInterface {
         this.container1.addEventListener('mouseenter', this.mouseEnter, true);
         this.container1.addEventListener('mouseleave', this.mouseLeave, true);
       }
-    }
-  };
-
-  initializeGameController(playerName) {
-    this.gameController = new GameController(playerName);
-    // this.gameController.player1.board.placeShip([0, 9], 3, true);
-    // this.gameController.player1.board.placeShip([0, 0], 2, true);
-    // this.gameController.player1.board.placeShip([6, 3], 4, false);
-    // this.gameController.player1.board.placeShip([4, 5], 5, false);
-
-    // this.gameController.cpu.board.placeShip([0, 9], 3, true);
-    // this.gameController.cpu.board.placeShip([0, 0], 2, true);
-    // this.gameController.cpu.board.placeShip([6, 3], 4, false);
-    // this.gameController.cpu.board.placeShip([4, 5], 5, false);
-
-    const player1Name = this.gameController.player1.name;
-    const player2Name = this.gameController.player2.name;
-
-    const playerBoards = [
-      this.gameController.player1.board.board,
-      this.gameController.cpu.board.board,
-    ];
-
-    return { player1Name, player2Name, playerBoards };
-  }
-
-  submitPlayerName(event) {
-    // Check if the input is empty
-    event.preventDefault();
-    if (!this.input.value.trim()) {
-      this.description.classList.remove('hidden');
-      this.description.textContent = 'Please enter a valid name';
-      return;
-    }
-
-    // Proceed with the form submission logic
-    this.playerTitle.textContent = this.input.value;
-    this.container1.classList.remove('hidden');
-    this.playerTitle.classList.remove('hidden');
-    this.form.classList.add('hidden');
-    this.aside.classList.remove('hidden');
-    this.description.textContent = '';
-    return this.input.value; // eslint-disable-line
-  }
-
-  createGrids(boards) {
-    boards.forEach((board, index) => {
-      const container = index === 0 ? this.container1 : this.container2;
-      container.dataset.value =
-        index === 0 ? this.player1Name : this.player2Name;
-      for (let i = 0; i < board.length; i += 1) {
-        for (let j = 0; j < board.length; j += 1) {
-          const grid = document.createElement('div');
-          grid.classList.add('grid');
-          // indexing the user interface to reference the players board with event handlers later
-          grid.dataset.value = `[${i},${j}]`;
-          grid.style.gridTemplateColumns = `repeat(${board.length}, auto)`;
-          if (board[i][j] instanceof BattleShip) {
-            grid.classList.add('ship');
-          }
-          container.appendChild(grid);
-        }
-      }
-    });
-  }
-
-  startUserInterface = (event) => {
-    event.preventDefault();
-    console.log(this.startBtn);
-    console.log(this.placedShipsUI.length);
-    console.log(this.placedShipsUI);
-
-    // going to create AI logic first to then pass both configs to GameController
-    // if (this.placedShipsUI.length === 4) {
-    //   this.placedShipsUI.forEach((ship) => {
-    //     console.log(ship.coords);
-    //     console.log(ship.coords[0]);
-    //     const [row, col] = ship.coords[0];
-    //     console.log('row', row);
-    //     console.log('col', col);
-
-    //     this.gameController.player1.board.placeShip(
-    //       [row, col],
-    //       ship.size,
-    //       ship.axis,
-    //     );
-    //   });
-    // }
-    this.startBtn.classList.add('hidden');
-    this.container2.classList.remove('hidden');
-    this.player2Title.classList.remove('hidden');
-    this.options.classList.add('hidden');
-  };
-
-  resetUserInterface = (event) => {
-    event.preventDefault();
-    this.startBtn.classList.toggle('hidden');
-    this.container2.classList.toggle('hidden');
-    this.player2Title.classList.toggle('hidden');
-    this.description.textContent = '';
-    this.resetBtn.classList.toggle('hidden');
-    // this.container1.classList.toggle('hidden');
-    // this.options.classList.toggle('hidden');
-  };
-
-  gameOverUserInterface(attacker) {
-    this.description.textContent = `Game over, ${attacker.name} wins!`;
-    this.options.classList.remove('hidden');
-    this.resetBtn.classList.remove('hidden');
-    removeListener('click', this.container1, this.AttackCoordinator);
-    removeListener('click', this.container2, this.AttackCoordinator);
-  }
-
-  displayAttackResult(results, event) {
-    const { attack, attacker, fleetSunk, ship } = results;
-    this.description.textContent = attack;
-    if (attack === 'you already targeted this position, try again') {
-      console.log('move this clause');
-    } else if (attack === 'Successful hit') {
-      event.target.classList.add('hit'); // orange
-    } else if (attack === 'Ship sinking') {
-      // traverse the positions of the sinking ship to update the UI(red)
-      ship.positions.forEach((pos) => {
-        const [r, c] = pos;
-        const container = event.target.parentNode.id;
-        const gridEl = document.querySelector(
-          `#${container} [data-value="[${r},${c}]"]`,
-        );
-        gridEl.classList.add('sunk');
-      });
-    } else {
-      event.target.classList.add('miss');
-    }
-    if (fleetSunk) {
-      this.gameOverUserInterface(attacker);
-    }
-  }
-
-  AttackCoordinator = (event) => {
-    if (event.target.classList.contains('grid')) {
-      const index = event.target.dataset.value;
-      const playerName = event.target.parentNode.dataset.value;
-      const [row, column] = JSON.parse(index);
-      const results = this.gameController.attack(playerName, row, column);
-      this.displayAttackResult(results, event);
     }
   };
 }
